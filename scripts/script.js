d12b854d88817fd03db14f5dbb21d03f90fa65a5
AnnKomissarova <nnkmssrv@gmail.com>
2023-08-23 13:53:28 +0300

    Added-modal-adaptive

--- scripts/script.js
+++ scripts/script.js
@@ -4,23 +4,4 @@ likeBtns.forEach((button) => {
   button.addEventListener("click", () => {
     button.classList.toggle("filled");
   });
-});
-  
-// const productCards = document.querySelectorAll("#product-cards");
-// const blockBtn = document.querySelector("#blocks-btn");
-// const ulListBtn = document.querySelector("#ul-list-btn");
-
-// productCards.forEach((cards) => {
-//   blockBtn.addEventListener("click", () => {
-//     cards.classList.remove("row-cols-md-1");
-//     cards.classList.add("row-cols-md-4");
-//     });  
-// });
-
-// productCards.forEach((cards) => {
-//   ulListBtn.addEventListener("click", () => {
-//     cards.classList.remove("row-cols-md-4");
-//     cards.classList.add("row-cols-md-1");
-//   });
-// });
-
+});  
